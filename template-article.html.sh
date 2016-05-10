965fb7ad061853021a48534c0c424b6c83591a10
Kimiaki Kuno <knokmki612@gmail.com>
2016-05-10 15:03:18 +0900

    記事の共有ボタンのG+をはてブに変更

--- template-article.html.sh
+++ template-article.html.sh
@@ -7,7 +7,7 @@ if \
 		<div class="social-icon">
 			<a href="http://twitter.com/share?url=${URL}post/$post&text=$title_encoded$TITLE_TAIL_ENCODED"><span class="icon-">twitter</span></a>
 			<a href="http://www.facebook.com/sharer.php?u=${URL}post/$post"><span class="icon-">facebook</span></a>
-			<a href="https://plusone.google.com/_/+1/confirm?hl=ja&url=${URL}post/$post"><span class="icon-">google+</span></a>
+			<a href="http://b.hatena.ne.jp/entry/${URL}post/$post"><span class="icon-">hatebu</span></a>
 		</div>
 		<div class="date">
 			<time datetime="$datetime">$formatted_date</time>
