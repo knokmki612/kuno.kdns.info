7b33ad985b7c81f121e19cb5aedd05298de2bd25
Kimiaki Kuno <knokmki612@gmail.com>
2016-05-19 00:30:52 +0900

    記事テンプレートのインデント変更

--- template-article.html.sh
+++ template-article.html.sh
@@ -4,17 +4,17 @@ if \
 	[ -n "$title_encoded" ]; then
 	cat <<- +
 		<aside class="clearfix">
-		<div class="social-icon">
-			<a href="http://twitter.com/share?url=${URL}post/$post&text=$title_encoded$TITLE_TAIL_ENCODED"><span class="icon-">twitter</span></a>
-			<a href="http://www.facebook.com/sharer.php?u=${URL}post/$post"><span class="icon-">facebook</span></a>
-			<a href="http://b.hatena.ne.jp/entry/${URL}post/$post"><span class="icon-">hatebu</span></a>
-		</div>
-		<div class="date">
-			<time datetime="$datetime">$formatted_date</time>
-		</div>
-		<div class="labels">
-			$labels_string
-		</div>
+		  <div class="social-icon">
+		  <a href="http://twitter.com/share?url=${URL}post/$post&text=$title_encoded$TITLE_TAIL_ENCODED"><span class="icon-">twitter</span></a>
+		  <a href="http://www.facebook.com/sharer.php?u=${URL}post/$post"><span class="icon-">facebook</span></a>
+		  <a href="http://b.hatena.ne.jp/entry/${URL}post/$post"><span class="icon-">hatebu</span></a>
+		  </div>
+		  <div class="date">
+		  <time datetime="$datetime">$formatted_date</time>
+		  </div>
+		  <div class="labels">
+		  $labels_string
+		  </div>
 		</aside>
 		<article>
 		<h2><a href="${URL}post/$post">$title</a></h2>
