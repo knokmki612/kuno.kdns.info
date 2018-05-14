96625cdb7fd3ecd0cb0dc78c1cfbc64281a23e6f
Kimiaki Kuno <knokmki612@gmail.com>
2018-05-14 23:18:48 +0900

    Migrate to hugo

--- jobs/wget_link_files.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-wget https://vivaldi.com/buttons/files/230x50.png -O cache/vivaldi.png
-wget https://secure.gravatar.com/avatar/6b2fd17bf8572ea5d60c0916db36627c.jpg?s=240 -O cache/avatar.jpg
